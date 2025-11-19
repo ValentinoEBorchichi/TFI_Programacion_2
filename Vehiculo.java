5d34bcfe41825964141a97322577efb211cf4578
Joel <maxecheverri9@gmail.com>
2025-11-19 15:17:40 -0300

    Mis cambios antes del merge

--- Vehiculo.java
+++ Vehiculo.java
@@ -13,4 +13,36 @@ abstract public class Vehiculo {
         this.Anio = anio;
         this.Estado = estado;
     }
+
+    //getters y setters
+    public int getPatente() {
+        return Patente;
+    }
+    public void setPatente(int patente) {
+        Patente = patente;
+    }
+    public String getMarca() {
+        return Marca;
+    }
+    public void setMarca(String marca) {
+        Marca = marca;
+    }
+    public String getModelo() {
+        return Modelo;
+    }
+    public void setModelo(String modelo) {
+        Modelo = modelo;
+    }
+    public int getAnio() {
+        return Anio;
+    }
+    public void setAnio(int anio) {
+        Anio = anio;
+    }
+    public String getEstado() {
+        return Estado;
+    }
+    public void setEstado(String estado) {
+        Estado = estado;
+    }
 }
