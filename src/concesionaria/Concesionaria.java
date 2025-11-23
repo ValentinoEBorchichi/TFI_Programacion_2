174ff5d6f83c760207294884dd67d6a6683dee03
FabioIgnacioNavarro <fabionavarroagios@gmail.com>
2025-11-23 18:11:21 -0300

    añadidos cambios de otras ramas a la rama main

--- src/concesionaria/Concesionaria.java
+++ src/concesionaria/Concesionaria.java
@@ -2,8 +2,13 @@ package concesionaria;
 
 import java.util.ArrayList;
 
+
 public class Concesionaria {
 
     public static ArrayList<Auto> listaAutos = new ArrayList<>();
     public static ArrayList<Mantenimiento> listaMantenimientos = new ArrayList<>();
+
+
+
+
 }
